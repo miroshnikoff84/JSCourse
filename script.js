cd3b01589527eb32faaf142cebb2f86529dc843e
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-01-23 21:09:57 +0300

    Create new project

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('hello');
+console.log('hello');
+
+var money = 1000000;
+var income = 45324;
+const addExpenses = 798;
+let deposit = 546356;
+const mission = 56367;
+var period = 56326;
+
+
