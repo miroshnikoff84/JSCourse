3a066a75975ab05d07b468f766e3a9bf17fa1351
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-01-27 09:20:22 +0300

    create branch lesson02

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-alert('hello');
-console.log('hello');
+var money = 90000;
+var income = 'freelance';
+let addExpenses = 'Internet, Taxi, Costs';
+var deposit = true;
+var mission = '5000000';
+var period = '11';
 
-var money = 1000000;
-var income = 45324;
-const addExpenses = 798;
-let deposit = 546356;
-const mission = 56367;
-var period = 56326;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
 
-
+var budgetDay = money / 30;
+console.log(budgetDay); 
